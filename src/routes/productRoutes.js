f5f23ceb4348d5245784b6a297c3efbd6837c39e
hannndii <shiirogami95@gmail.com>
2025-12-27 09:03:18 +0700

    update

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -7,8 +7,8 @@ const roleMiddleware = require('../middlewares/roleMiddleware');
 
 router.get('/', productController.getAllProducts);
 router.get('/:id', productController.getProductById);
-router.post('/',authMiddleware, roleMiddleware('admin'), productController.createProduct);
-router.put('/:id',authMiddleware, roleMiddleware('admin'), productController.updateProduct);
-router.delete('/:id',authMiddleware, roleMiddleware('admin'), productController.deleteProduct);
+router.post('/products',authMiddleware, roleMiddleware('admin'), productController.createProduct);
+router.put('/products/:id',authMiddleware, roleMiddleware('admin'), productController.updateProduct);
+router.delete('/products/:id',authMiddleware, roleMiddleware('admin'), productController.deleteProduct);
 
 module.exports = router;
