fd2ae197dfa15a27be13829746218aaa591a47c3
hannndii <shiirogami95@gmail.com>
2026-05-07 14:06:12 +0700

    update

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const ProductControllers = require ('../controllers/productControllers');
 const authMiddleware = require('../middlewares/authMiddleware');
 const roleMiddleware = require('../middlewares/roleMiddleware');
+<<<<<<< Updated upstream
 const upload = require('../middlewares/uploadMiddleware');
 
 
@@ -11,5 +12,15 @@ router.get('/:id', ProductControllers.getProductById);
 router.post('/', authMiddleware, roleMiddleware('admin'), upload.single('product_image'), ProductControllers.createProduct);
 router.put('/:id', authMiddleware, roleMiddleware('admin'), upload.single('product_image'), ProductControllers.updateProduct);
 router.delete('/:id', authMiddleware, roleMiddleware('admin'), ProductControllers.deleteProduct);
+=======
+ 
+
+// kasih throttle biar ga kena ddos attack, misal 100 request per 15 menit
+router.get('/', productController.getAllProducts);
+router.get('/:id', productController.getProductById);
+router.post('/',authMiddleware, roleMiddleware('admin'), productController.createProduct);
+router.put('/:id',authMiddleware, roleMiddleware('admin'), productController.updateProduct);
+router.delete('/:id',authMiddleware, roleMiddleware('admin'), productController.deleteProduct);
+>>>>>>> Stashed changes
 
 module.exports = router;
